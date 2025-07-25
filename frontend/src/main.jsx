8c5f4b5b258830060bc386e3d25a2d7da8ef5406
Maximo Dimas <156684286+MaxDimasDev@users.noreply.github.com>
2025-07-25 00:19:10 -0600

    change: Complete structure of "main.jsx"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/global.css' // Cambia esta línea
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
